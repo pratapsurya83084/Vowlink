d09646e6b19ce9ad8a804ed24a0aa953fe15a30d
Pratap Suryawanshi <pratapsuryawanshi007@gmail.com>
2024-07-19 00:53:38 +0530

    add new sections

--- vowlink/src/App.jsx
+++ vowlink/src/App.jsx
@@ -3,8 +3,7 @@
 import { BrowserRouter as Router,Routes ,Route} from 'react-router-dom'
 //import all component and make route in this coponent
 import Home from './components/home/Home'
-import About  from './components/about/About'
-import Contact from './components/contact/Contact'
+
 import Frachise from './components/franchise/Franchise'
 function App() {
  
@@ -13,8 +12,7 @@ function App() {
     <Router>
       <Routes>
         <Route path='/' element={<Home/>}/>
-        <Route path='/about' element={<About/>}/>
-        <Route path='/contact' element={<Contact/>}/>
+       
         <Route path='/franchise' element={<Frachise/>}/>
       </Routes>
     </Router>
--- vowlink/src/components/home/Home.jsx
+++ vowlink/src/components/home/Home.jsx
@@ -3,17 +3,16 @@ import Layout from "../../components/layout/Layout";
 // import Cards from '../pages/homepages/Cards'
 import Faq  from '../homePages/FaqSection'
 import TopPoster   from '../homePages/TopPoster'
-import CourseCards   from '../homePages/CourseCards'
 import AboutSections  from '../homePages/AboutSections'
 import Testimonials  from '../homePages/Testimonials'
 import PlacementReport  from '../homePages/PlacementReport'
-
+import ImageAndTextSection  from  '../homePages/ImageAndTextSection'
 const Home = () => {
   return (
     <Layout>
       <div>
         <TopPoster/>
-        <CourseCards/>
+        <ImageAndTextSection/>
        
         {/* <Cards/> */}
         <AboutSections/>
