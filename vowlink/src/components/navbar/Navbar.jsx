c5128fe2406f7d2c1f20c5e099e8ee960d374e23
Pratap Suryawanshi <pratapsuryawanshi007@gmail.com>
2024-07-31 19:33:11 +0530

    add logo of vl

--- vowlink/src/components/navbar/Navbar.jsx
+++ vowlink/src/components/navbar/Navbar.jsx
@@ -1,4 +1,3 @@
-
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Dialog, DialogPanel } from "@headlessui/react";
@@ -11,60 +10,62 @@ const Navbar = () => {
     <header>
       {/* fixed */}
       <nav
-  aria-label="Global"
-  // bg-gray-50
-  className="font-type bg-[#0c9db3] text-white   shadow-lg flex items-center p-7 lg:px-8
+        aria-label="Global"
+        // bg-gray-50
+        className="font-type bg-[#0c9db3] text-white    flex items-center
+   p-2 lg:px-8
     top-0 w-full"
->
-  {/* Logo */}
-  <div className="flex flex-1 items-center">
-    <Link to="/" className="-m-1.5 p-1.5">
-      <span className="sr-only ">vowlink Company</span>
-      vowlink logo
-    </Link>
-  </div>
+      >
+        {/* Logo */}
+        {/* items-center */}
+        <div className="flex flex-1  ">
+          <Link to="/" className=" ">
+            <span className="sr-only ">vowlink Company</span>
+            {/* vowlink logo */}
+            <img
+              className="h-24"
+              src="\public\vowlink_logo_header.png"
+              alt=""
+            />
+          </Link>
+        </div>
 
- 
+        {/* Large menu links */}
+        {/* lg:mr-[400px]  */}
+        <div className="hidden m-3 lg:flex lg:flex-1  lg:gap-x-12">
+          <Link to="/" className="hover:text-gray-200 text-xl">
+            Home
+          </Link>
 
-  {/* Large menu links */}
-  {/* lg:mr-[400px]  */}
-  <div className="hidden m-3 lg:flex lg:flex-1  lg:gap-x-12">
-    <Link to="/" 
-    className="hover:text-gray-200 text-xl">
-      Home
-    </Link>
-   
-    <Link to="/franchise" 
-    className="hover:text-gray-200 text-xl">
-      Franchise
-    </Link>
-  </div>
+          <Link to="/franchise" className="hover:text-gray-200 text-xl">
+            Franchise
+          </Link>
+        </div>
 
-  {/* button */}
-  <div className=" flex gap-2 sm:gap-10">
-  <div className="hidden md:flex">
-  <a href="https://bisjhintus.com/">
-                <button className="bg-white hover:bg-gray-100 focus:bg-slate-100  text-black px-5 py-3 rounded text-lg">Visit Global</button>
-                </a> 
-  </div>
+        {/* button */}
+        <div className=" flex gap-2 sm:gap-10">
+          <div className="hidden md:flex">
+            <a href="https://bisjhintus.com/">
+              <button className="bg-white hover:bg-gray-100 focus:bg-slate-100  text-black px-5 py-3 rounded text-lg">
+                Visit Global
+              </button>
+            </a>
+          </div>
 
-
- {/* Mobile menu button */}
- <div className="flex lg:hidden ">
-    <button
-      type="button"
-      onClick={() => setMobileMenuOpen(true)}
-      // text-gray-700
-      className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5">
-      <span className="sr-only">Open main menu</span>
-      <Bars3Icon aria-hidden="true" className="h-9 w-9 " />
-    </button>
-  </div>
-  </div>
-  
-
-</nav>
-
+          {/* Mobile menu button */}
+          <div className="flex lg:hidden ">
+            <button
+              type="button"
+              onClick={() => setMobileMenuOpen(true)}
+              // text-gray-700
+              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5"
+            >
+              <span className="sr-only">Open main menu</span>
+              <Bars3Icon aria-hidden="true" className="h-9 w-9 " />
+            </button>
+          </div>
+        </div>
+      </nav>
 
       {/* Mobile menu open navbar*/}
       <Dialog
@@ -76,12 +77,12 @@ const Navbar = () => {
         <DialogPanel className="fixed  inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between ">
             <Link to="/" className="-m-1.5 p-1.5">
-              vowlink logo
-              {/* <img
+              {/* vowlink logo */}
+              <img
                 alt=""
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                className="h-8 w-auto"
-              /> */}
+                src="\vowlink_logo_header.png"
+                className="h-24 w-auto "
+              />
             </Link>
             <button
               type="button"
@@ -102,7 +103,7 @@ const Navbar = () => {
                 >
                   Home
                 </Link>
-                
+
                 <Link
                   to="/franchise"
                   className="-mx-3 block font-jost rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
@@ -112,9 +113,11 @@ const Navbar = () => {
 
                 {/* button show in phone size and medium size hidden*/}
                 <div className="md:hidden flex">
-                <a href="https://bisjhintus.com/">
-                <button className="bg-cyan-500 text-white px-5 py-3 rounded text-lg">Visit Global</button>
-                </a> 
+                  <a href="https://bisjhintus.com/">
+                    <button className="bg-cyan-500 text-white px-5 py-3 rounded text-lg">
+                      Visit Global
+                    </button>
+                  </a>
                 </div>
               </div>
             </div>
